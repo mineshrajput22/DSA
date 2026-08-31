2bd5ea0acf9f5976dd7f9a204b2af5f679c15cdb
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2026-08-31 12:54:00 +0530

    add groupAnagrams function to group strings by anagrams

---
+++ LeetCode/groupAnagrams.ts
@@ -0,0 +1,15 @@
+function groupAnagrams(strs: string[]) {
+	const map = new Map();
+
+	for (const str of strs) {
+		const key = str.split('').sort().join('');
+
+		if (!map.has(key)) {
+			map.set(key, []);
+		}
+
+		map.get(key).push(str);
+	}
+
+	return Array.from(map.values());
+}
