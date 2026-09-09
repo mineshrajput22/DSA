d8e8d9bd5f9678429f6d37f85016e156958b585a
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2026-09-09 11:44:31 +0530

    add validAnagram2 function to check if two strings are anagrams using character count

---
+++ LeetCode/topKFrequent.ts
@@ -0,0 +1,36 @@
+function topKFrequent(nums: number[], k: number) {
+	const counter = new Map<number, number>();
+
+	for (const num of nums) {
+		counter.set(num, (counter.get(num) || 0) + 1);
+	}
+
+	const bucket: number[][] = Array.from(
+		{
+			length: nums.length + 1,
+		},
+		() => [],
+	);
+
+	for (const [num, frequency] of counter.entries()) {
+		bucket[frequency]?.push(num);
+	}
+
+	const result: number[] = [];
+
+	for (let i = bucket.length - 1; i >= 0 && result.length < k; i--) {
+		for (const num of bucket[i]!) {
+			result.push(num);
+
+			if (result.length === k) break;
+		}
+	}
+
+	return result;
+}
+
+const nums = [1, 1, 1, 2, 2, 3];
+let k = 2;
+
+
+console.log(topKFrequent(nums, k));
--- LeetCode/validAnagram.ts
+++ LeetCode/validAnagram.ts
@@ -19,4 +19,25 @@ function validAnagram(string: string, target: string) {
 	return true;
 }
 
+function validAnagram2(string: string, target: string) {
+	if (string.length !== target.length) {
+		return false;
+	}
+
+	const charMap = new Map();
+
+	for (let i = 0; i < string.length; i++) {
+		charMap.set(string[i], (charMap.get(string[i]) || 0) + 1);
+		charMap.set(target[i], (charMap.get(target[i]) || 0) - 1);
+	}
+
+	//if the are anagrams all the values will be zero
+	for (const count of charMap.values()) {
+		if (count !== 0) return false;
+	}
+
+	return true;
+}
+
 console.log(validAnagram('anagram', 'nagaram'));
+console.log(validAnagram2('anagram', 'nagaram'));
